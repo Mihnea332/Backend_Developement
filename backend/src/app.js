f2ad133c33f12772f3fafb046f811fc1f390fac9
Kaneki3332 <gabi.mihnea46@gmail.com>
2026-02-27 13:25:17 +0200

    CRUD APIS

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/post", postRouter);
 // example route: http://localhost:4000/api/v1/users/register
 export default app;
